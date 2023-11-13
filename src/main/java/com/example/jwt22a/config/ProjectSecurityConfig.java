7b5a7d5264c835e0dc565abc14426b35c48f2763
Hjordten <andr314e@stud.kea.dk>
2023-11-13 11:31:32 +0100

    hej

--- src/main/java/com/example/jwt22a/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/jwt22a/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,21 +14,20 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            requests.requestMatchers("/myAccount","/myBalance").authenticated()
-                    .requestMatchers("/contact").permitAll();
-        });
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        // var obj = (SecurityFilterChain)http.build();  debug her for at se indbyggede filtre i Spring Security
-        return (SecurityFilterChain)http.build();
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount", "/myBalance").authenticated()
+                        .requestMatchers("/contact", "/register").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        //return NoOpPasswordEncoder.getInstance(); ingen kryptering
+        return new BCryptPasswordEncoder(20);
     }
-
 }
 
