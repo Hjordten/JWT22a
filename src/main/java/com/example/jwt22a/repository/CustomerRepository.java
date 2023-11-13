f4063acbd3da5cf44f7dd63b7f44a01178f6d000
erlm <erlm@kea.dk>
2023-11-13 10:42:21 +0100

    register user

---
+++ src/main/java/com/example/jwt22a/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.jwt22a.repository;
+
+import com.example.jwt22a.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    Optional<Customer> findByEmail(String email);
+
+}
